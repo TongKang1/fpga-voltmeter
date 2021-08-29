ffda9ddf34ab65dbcaf429eeaa3d6e9f72bd73e6
Rock K <35797048+TongKang1@users.noreply.github.com>
2021-08-29 20:40:14 +0800

    Add files via upload

---
+++ spi_oled.v
@@ -0,0 +1,306 @@
+module spi_oled(
+	input clk_in,
+	input rst_n_in,
+	input [7:0]rd_data,
+	output oled_csn,
+	output oled_rst,
+	output oled_dcn,
+	output oled_clk,
+	output oled_dat,
+	output [8:0]rd_addr,
+	output rd_en
+);
+	
+	localparam SPI_CLK = 5'd30;
+	reg [4:0]count_spi;
+	reg clk_spi;
+	reg [4:0] count_spi;
+	assign oled_rst = rst_n_in;
+	wire oled_csn;
+	reg oled_dcn;
+	reg oled_clk;
+	wire oled_dat;
+	reg spi_csn;
+	reg rd_en;
+	reg [8:0]rd_addr;
+	
+	localparam PERIOD = 18'd200_000;
+	reg [7:0]count_sys;
+	reg [7:0]cmd_test;
+	reg led;
+	reg start;
+	
+	write_8bit write_byte(
+		.csn_in(spi_csn),
+		.clk(clk_spi),
+		.data_in(cmd_test),
+		.data_out(oled_dat),
+		.csn_out(oled_csn)
+	);	
+
+	
+	always@(posedge clk_in or negedge rst_n_in)
+		begin
+			if(!rst_n_in)
+				begin
+					clk_spi <= 1'b0;
+					count_spi <= 5'b0;
+					oled_clk <= 1'b1;
+				end
+			else
+				begin
+					if(count_spi==SPI_CLK)
+						begin
+							count_spi <= 5'b0;
+							clk_spi <= ~clk_spi;
+							oled_clk <= ~oled_clk;
+							
+						end
+					else
+						begin
+							count_spi <= count_spi+1;
+						end
+				end
+		end
+	
+	reg [1:0]count_loop_1;
+	reg [3:0]count_loop_2;
+	reg [7:0]page_addr;
+	
+	always@(posedge clk_spi or negedge rst_n_in)
+		begin
+			if(!rst_n_in)
+				begin
+					count_sys <= 8'b0;
+					spi_csn <= 1'b1;
+					oled_dcn <= 1'b0;
+					//oled_clk <= 1'b0;
+					cmd_test <= 8'hAE;
+					led <= 1'b0;
+					start <= 1'b0;
+					count_loop_1 <= 2'b0;
+					count_loop_2 <= 4'b0;
+					page_addr <= 8'hB0;
+					rd_addr <= 9'b0;
+					rd_en <= 1'b0;
+				end
+			else
+				case(start)
+					//ssd1306 initial cmd
+					1'b0: begin
+						case(count_sys)
+							8'd0:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd8:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+									cmd_test <= 8'hA8;
+								end
+								
+							8'd10:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd18:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+									cmd_test <= 8'h1F;
+								end
+							
+							8'd20:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd28:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+									cmd_test <= 8'hDA;
+								end
+							8'd30:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd38:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+									cmd_test <= 8'h02;
+								end
+							
+							8'd40:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd48:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+									cmd_test <= 8'h8D;
+								end
+							
+							8'd50:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd58:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+									cmd_test <= 8'h14;
+								end
+							
+							8'd60:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd68:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+									cmd_test <= 8'hAF;
+								end
+							
+							8'd70:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b0;
+									oled_dcn <= 1'b0;
+								end
+							8'd78:  begin
+									count_sys <= count_sys+1;
+									spi_csn <= 1'b1;
+									oled_dcn <= 1'b0;
+								end
+							
+							8'd80: begin
+									start <= start+1;
+									count_sys <= 8'b0;
+								end
+							default: begin
+									count_sys <= count_sys+1;
+								end
+						endcase
+					end
+				1'b1: begin
+					//display from ram
+						case(count_loop_1)
+							2'd0: begin
+									case(count_loop_2)
+										4'd0: begin
+												cmd_test <= page_addr;
+												spi_csn <= 1'b0;
+												oled_dcn <= 1'b0;
+												count_loop_2 <= count_loop_2+1;
+											end
+										4'd8: begin
+												spi_csn <= 1'b1;
+												count_loop_2 <= count_loop_2+1;
+											end
+										4'd10: begin
+												count_loop_1 <= count_loop_1+1;
+												count_loop_2 <= 4'd0;
+											end
+										default: begin
+												count_loop_2 <= count_loop_2+1;
+											end
+									endcase
+								end
+							2'd1: begin
+									case(count_loop_2)
+										4'd0: begin
+												cmd_test <= 8'h00;
+												spi_csn <= 1'b0;
+												oled_dcn <= 1'b0;
+												count_loop_2 <= count_loop_2+1;
+											end
+										4'd8: begin
+												spi_csn <= 1'b1;
+												count_loop_2 <= count_loop_2+1;
+											end
+										4'd10: begin
+												count_loop_1 <= count_loop_1+1;
+												count_loop_2 <= 4'd0;
+											end
+										default: begin
+												count_loop_2 <= count_loop_2+1;
+											end
+									endcase
+								end
+							2'd2: begin
+									case(count_loop_2)
+										4'd0: begin
+												cmd_test <= 8'h10;
+												spi_csn <= 1'b0;
+												oled_dcn <= 1'b0;
+												count_loop_2 <= count_loop_2+1;
+											end
+										4'd8: begin
+												spi_csn <= 1'b1;
+												count_loop_2 <= count_loop_2+1;
+											end
+										4'd10: begin
+												count_loop_1 <= count_loop_1+1;
+												count_loop_2 <= 4'd0;
+												rd_addr[6:0] <= 7'b0;
+											end
+										default: begin
+												count_loop_2 <= count_loop_2+1;
+											end
+									endcase
+								end
+							2'd3: begin
+									case(count_sys)
+										8'd128: begin
+												count_sys <= 8'd0;
+												page_addr[1:0] <= page_addr[1:0]+1;
+												rd_addr[8:7] <= rd_addr[8:7]+1;
+												rd_addr[6:0] <= 7'b0;
+												count_loop_1 <= count_loop_1+1;
+											end
+										default: begin
+												case(count_loop_2)
+													4'd0: begin
+															rd_en <= 1'b1;
+															count_loop_2 <= count_loop_2+1;
+														end
+													4'd1: begin
+															cmd_test <= rd_data;
+															spi_csn <= 1'b0;
+															oled_dcn <= 1'b1;
+															count_loop_2 <= count_loop_2+1;
+														end
+													
+													4'd9: begin
+															spi_csn <= 1'b1;
+															rd_en <= 1'b0;
+															rd_addr[6:0] <= rd_addr[6:0]+1;
+															count_loop_2 <= count_loop_2+1;
+														end
+													4'd10: begin
+															count_sys <= count_sys+1;
+															count_loop_2 <= 4'd0;
+														end
+													default: begin
+															count_loop_2 <= count_loop_2+1;
+														end
+												endcase
+											end
+									endcase
+								end
+						endcase
+					end
+				endcase
+		end
+
+endmodule
---
+++ volt_display.v
@@ -0,0 +1,318 @@
+module volt_display(
+	input clk_in,
+	input rst_n_in,
+	input [7:0]BCD,
+	output wr_en,
+	output [8:0]wr_addr,
+	output [7:0]wr_data	
+);
+	
+	reg [127:0]oled_num [9:0];
+	reg [127:0]oled_O;
+	reg [127:0]oled_L;
+	reg [127:0]oled_E;
+	reg [127:0]oled_A;
+	reg [127:0]oled_D;
+	reg [127:0]oled_C;
+	reg [127:0]oled_dot;
+	reg [127:0]oled_v;
+	
+	reg [7:0]wr_data;
+	reg [8:0]wr_addr;
+	reg wr_en;
+
+	reg start;
+	reg [6:0]index;
+	reg [3:0]state;
+	reg [1:0]loop_1;
+	reg [4:0]loop_2;
+	reg [1:0]loop_3;
+	reg [8:0]add;
+	reg [127:0]data_display;
+	
+	always@(posedge clk_in or negedge rst_n_in)
+		begin
+			if(!rst_n_in)
+				begin
+					start <= 1'b0;
+					
+					wr_en <= 1'b0;
+					wr_data <= 8'b0;
+					wr_addr <= 9'b0;					
+
+					oled_num[0] <= 128'hF00FF81F081288114810F81FF00F0000;
+					oled_num[1] <= 128'h000020103010F81FF81F001000100000;
+					oled_num[2] <= 128'h101C181E08138811C810781830180000;
+					oled_num[3] <= 128'h10081818881088108810F81F700F0000;
+					oled_num[4] <= 128'h8001C00160013011F81FF81F00110000;
+					oled_num[5] <= 128'hF808F818881088108811881F080F0000;
+					oled_num[6] <= 128'hE00FF01F981088108810801F000F0000;
+					oled_num[7] <= 128'h18001800081E081F8801F80078000000;
+					oled_num[8] <= 128'h700FF81F881088108810F81F700F0000;
+					oled_num[9] <= 128'h7000F810881088108818F80FF0070000;
+					
+					oled_O <= 128'hE007F00F181808101818F00FE0070000;
+					oled_L <= 128'h0810F81FF81F081000100018001C0000;
+					oled_E <= 128'h0810F81FF81F8810C8111818381C0000;
+					oled_A <= 128'hC01FE01F300118013001E01FC01F0000;
+					oled_D <= 128'h0810F81FF81F08101818F00FE0070000;
+					oled_C <= 128'hE007F00F1818081008101818300C0000;
+					oled_dot <= 128'h00000000000000180018000000000000;
+					oled_v <= 128'h0000C007C00F00180018C00FC0070000;
+					
+					index <= 7'b0;
+					state <= 4'b0;
+					loop_1 <= 2'b0;
+					loop_2 <= 5'b0;
+					loop_3 <= 2'b0;
+					data_display <= 128'b0;
+					add <= 9'b0_0000_0001;
+				end
+			else
+				if(!start)
+					//initial display
+					begin
+						case(loop_1)
+							2'd0: begin
+									case(state)
+										4'd0: begin
+												data_display <= oled_O;
+												wr_addr <= 9'd344;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												loop_2 <= 5'b0;
+												add <= 9'b0_0000_0001;
+											end
+										4'd1: begin
+												data_display <= oled_L;
+												wr_addr <= 9'd336;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd2: begin
+												data_display <= oled_E;
+												wr_addr <= 9'd328;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd3: begin
+												data_display <= oled_D;
+												wr_addr <= 9'd320;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd4: begin
+												data_display <= oled_A;
+												wr_addr <= 9'd304;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd5: begin
+												data_display <= oled_D;
+												wr_addr <= 9'd296;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd6: begin
+												data_display <= oled_C;
+												wr_addr <= 9'd288;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd7: begin
+												data_display <= oled_dot;
+												wr_addr <= 9'd64;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd8: begin
+												data_display <= oled_v;
+												wr_addr <= 9'd48;
+												index <= 7'd0;
+												state <= state+1;
+												loop_1 <= loop_1+1;
+												add <= 9'b0_0000_0001;
+											end
+										4'd9: begin
+												state <= 4'd0;
+												loop_1 <= 2'd0;
+												loop_2 <= 5'd0;
+												loop_3 <= 2'b0;
+												index <= 7'd0;
+												start <= 1'b1;
+												add <= 9'b0_0000_0001;
+											end
+									endcase
+								end
+							2'd1: begin
+									//write 128 bits in ram, according to address
+									case(loop_2)
+										5'd16: begin
+												loop_2 <= 5'd0;
+												loop_1 <= 2'd0;
+												index <= 7'd0;
+												add <= 9'b0_0000_0001;
+												//start <= 1'b1;
+											end
+										default: begin
+												case(loop_3)
+													2'd0: begin
+															
+															wr_data[7] <= data_display[index];
+															wr_data[6] <= data_display[index+1];
+															wr_data[5] <= data_display[index+2];
+															wr_data[4] <= data_display[index+3];
+															wr_data[3] <= data_display[index+4];
+															wr_data[2] <= data_display[index+5];
+															wr_data[1] <= data_display[index+6];
+															wr_data[0] <= data_display[index+7];
+															
+															wr_en <= 1'b1;
+															add[0] <= ~add[0];
+															loop_3 <= loop_3+1;
+														end
+													2'd1: begin
+															loop_3 <= loop_3+1;
+														end
+													2'd2: begin
+															wr_en <= 1'b0;
+															loop_3 <= loop_3+1;
+														end
+													2'd3: begin
+															wr_addr[7] <= wr_addr[7]+1;
+															wr_addr[6:0] <= wr_addr[6:0]+add[6:0];
+															index <= index+8;
+															loop_3 <= loop_3+1;
+															loop_2 <= loop_2+1;
+														end
+												endcase
+											end
+										
+									endcase
+								end
+						endcase	
+					end
+				else
+					//update real-time voltage
+					begin
+						case(loop_1)
+							2'd0: begin
+									data_display <= oled_num[BCD[7:4]];
+									wr_addr <= 9'd72;
+									add <= 1'b0;
+									loop_1 <= loop_1+1;
+									loop_2 <= 5'd0;
+									loop_3 <= 2'd0;
+									index <= 7'd0;
+								end
+							2'd1: begin
+									case(loop_2)
+										5'd16: begin
+												loop_2 <= 5'd0;
+												loop_1 <= loop_1+1;
+												index <= 7'd0;
+											end
+										default: begin
+												case(loop_3)
+													2'd0: begin
+															wr_data[7] <= data_display[index];
+															wr_data[6] <= data_display[index+1];
+															wr_data[5] <= data_display[index+2];
+															wr_data[4] <= data_display[index+3];
+															wr_data[3] <= data_display[index+4];
+															wr_data[2] <= data_display[index+5];
+															wr_data[1] <= data_display[index+6];
+															wr_data[0] <= data_display[index+7];
+															wr_en <= 1'b1;
+															add[0] <= ~add[0];
+															loop_3 <= loop_3+1;
+														end
+													2'd1: begin
+															loop_3 <= loop_3+1;
+														end
+													2'd2: begin
+															wr_en <= 1'b0;
+															loop_3 <= loop_3+1;
+														end
+													2'd3: begin
+															wr_addr[7] <= wr_addr[7]+1;
+															wr_addr[6:0] <= wr_addr[6:0]+add[6:0];
+															index <= index+8;
+															loop_3 <= loop_3+1;
+															loop_2 <= loop_2+1;
+														end
+												endcase
+											end
+									endcase
+								end
+								2'd2: begin
+									data_display <= oled_num[BCD[3:0]];
+									wr_addr <= 9'd56;
+									add <= 1'b0;
+									loop_1 <= loop_1+1;
+									loop_2 <= 5'd0;
+									loop_3 <= 2'd0;
+									index <= 7'd0;
+								end
+								2'd3: begin
+									case(loop_2)
+										5'd16: begin
+												loop_2 <= 5'd0;
+												loop_1 <= loop_1+1;
+												index <= 9'd0;
+											end
+										default: begin
+												case(loop_3)
+													2'd0: begin
+															wr_data[7] <= data_display[index];
+															wr_data[6] <= data_display[index+1];
+															wr_data[5] <= data_display[index+2];
+															wr_data[4] <= data_display[index+3];
+															wr_data[3] <= data_display[index+4];
+															wr_data[2] <= data_display[index+5];
+															wr_data[1] <= data_display[index+6];
+															wr_data[0] <= data_display[index+7];
+															wr_en <= 1'b1;
+															add[0] <= ~add[0];
+															loop_3 <= loop_3+1;
+														end
+													2'd1: begin
+															loop_3 <= loop_3+1;
+														end
+													2'd2: begin
+															wr_en <= 1'b0;
+															loop_3 <= loop_3+1;
+														end
+													2'd3: begin
+															wr_addr[7] <= wr_addr[7]+1;
+															wr_addr[6:0] <= wr_addr[6:0]+add[6:0];
+															index <= index+8;
+															loop_3 <= loop_3+1;
+															loop_2 <= loop_2+1;
+														end
+												endcase
+											end
+									endcase
+								end
+						endcase
+					end
+					
+		end
+
+
+endmodule
